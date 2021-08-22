558a4c2fae334f9f41c86fd275a69e0b9758838f
TMA2018 <michail.tal@inbox.ru>
2021-08-22 18:19:47 +0200

    add react monaco editor instead of textarea

--- src/components/custom-style/custom-style.js
+++ src/components/custom-style/custom-style.js
@@ -1,50 +1,57 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import debounce from 'lodash.debounce';
 import Spinner from '../spinner/spinner';
+import MonacoEditor from 'react-monaco-editor';
 import './custom-style.scss';
 
 function CustomStyle({template, onUpdateStyle, loading }) {
 
     const [value, onUpdateValue] = useState(template);
     //const debouncedUpdateStyle = debounce(onUpdateStyle, 5000);
-    const changeStyle = (evt) => {
-        onUpdateValue(evt.target.value);
-        onUpdateStyle(evt.target.value);
+    const changeStyle = (newvalue, evt) => {
+        onUpdateValue(newvalue);
+        onUpdateStyle(newvalue);
+        //console.log("onChange", newvalue, evt);
     }
     const debouncedChangeStyle = useMemo(
         () => debounce(changeStyle, 5000), [onUpdateStyle]
     );
 
-    //console.log('template', template);
-    //onUpdateValue(template);
-    //template = value;
     useEffect(() => {
         if (!value) onUpdateValue(template);
         console.log('value', value);
-        //debouncedChangeStyle();
-        // let timerId = setInterval(onUpdateStyle, 5000, value);
-        // return () => {
-        //     clearInterval(timerId);
-        // }
         return () => {
-            //debouncedUpdateStyle.cancel();
             debouncedChangeStyle.cancel();
         }
     }, [template, value, debouncedChangeStyle]);
-
-    //console.log('value', value);
     
     if (loading) {
         return <Spinner/>
     }
-    let arr = value.replace(/}/g,'}\n').split('\n');//value.replace(/}/g,'}\n');
+    /*let arr = value.replace(/}/g,'}\n').split('\n');//value.replace(/}/g,'}\n');
     let style = '';
     for (let str of arr) {
         style += str.trim() + '\n';
+    }*/
+    const options = {
+        selectOnLineNumbers: true,
+        fontSize: 10,
+        minimap: {
+            enabled: false
+        },
+        wordWrap: true
+    };
+
+    function editorDidMount(editor, monaco) {
+        console.log("editorDidMount", editor);
+        editor.focus();
     }
+
+    //const model = this.refs.monaco.editor.getModel();
+    //const value1 = model.getValue();
     return (
         <div className='customStyle'>
-            <textarea
+            {/* <textarea
                 className='customStyle__code'
                 name='style'
                 placeholder='write new style'
@@ -54,13 +61,26 @@ function CustomStyle({template, onUpdateStyle, loading }) {
                 rows = {20}
                 cols = {30}
                 onChange = {debouncedChangeStyle}
-            ></textarea>
-            <button 
+            ></textarea> */}
+            <MonacoEditor
+                width="400"
+                height="300"
+                language="css"
+                theme="vs-dark"
+                value={value}
+                options={options}
+                onChange={debouncedChangeStyle}
+                editorDidMount={editorDidMount}
+            />
+            {/* <button 
                 type='button' 
                 className='btn customStyle__btn'
-                onClick={ () => onUpdateStyle(style)}
+                onClick={ () => {
+                        onUpdateStyle(editor.getValue());
+                    }
+                }
             >Обновить стили
-            </button>
+            </button> */}
         </div>
     )
 }
